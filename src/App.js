907edfcdac19d772e2dbbdcf2920703ab2146112
JoeJorgensen <joebjorg@gmail.com>
2022-02-22 19:24:21 -0700

    lists

--- src/App.js
+++ src/App.js
@@ -1,14 +1,10 @@
 
-import "./App.css";
-import TodoList from "./TodoList";
 import ShowsList from './ShowsList'
 import './Lists.css';
 
 function App() {
   return (
     <div className = 'listStyle'>
-      <TodoList name='Coding To do list'/>
-      <TodoList name='Another List'/>
       <ShowsList name='Shows to Watch' />
     </div>
   );
